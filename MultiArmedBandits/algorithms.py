1594de931fd0162a4606409c5b6546d0ff049c6f
Vincent Mai <vincent.mai@umontreal.ca>
2019-02-07 20:04:26 -0500

    E-Greedy algorithms inherit from a base class
    Not tested yet

--- MultiArmedBandits/algorithms.py
+++ MultiArmedBandits/algorithms.py
@@ -409,7 +409,6 @@ class EpsilonGreedyAlgo():
         self.epsilon = self.epsilon_save
         self.est_means = [-math.inf for k in range(self.nb_arms)]
         self.received_rewards = [[] for k in range(self.nb_arms)]
-        self.received_rewards_weights = [[] for k in range(self.nb_arms)]
 
     def update(self, arm_id, reward, var):
         self.received_rewards[arm_id - 1].append(reward)
@@ -428,26 +427,28 @@ class EpsilonGreedyAlgo():
         return action, True
 
 
-class ModifiedEpsilonGreedyAlgo():
+class ModifiedEpsilonGreedyAlgo(EpsilonGreedyAlgo):
 
     def __init__(self, epsilon, decay_rate, omega):
-        self.r = 1
-        self.omega = omega
-        self.nb_arms = len(self.omega)
-        self.est_means = [-math.inf for k in range(self.nb_arms)]
-        self.received_rewards = [[] for k in range(self.nb_arms)]
+        super().__init__(epsilon, decay_rate, omega)
+        #self.r = 1
+        #self.omega = omega
+        #self.nb_arms = len(self.omega)
+        #self.est_means = [-math.inf for k in range(self.nb_arms)]
+        #self.received_rewards = [[] for k in range(self.nb_arms)]
         self.received_rewards_weights = [[] for k in range(self.nb_arms)]
-        self.epsilon = epsilon
-        self.decay_rate = decay_rate
-        self.epsilon_save = epsilon
-        self.minimal_epsilon = 0.01
+        #self.epsilon = epsilon
+        #self.decay_rate = decay_rate
+        #self.epsilon_save = epsilon
+        #self.minimal_epsilon = 0.01
         self.name = "Modified egreedy"
 
 
     def reset(self):
-        self.epsilon = self.epsilon_save
-        self.est_means = [-math.inf for k in range(self.nb_arms)]
-        self.received_rewards = [[] for k in range(self.nb_arms)]
+        EpsilonGreedyAlgo.reset(self)
+    #   self.epsilon = self.epsilon_save
+    #   self.est_means = [-math.inf for k in range(self.nb_arms)]
+    #   self.received_rewards = [[] for k in range(self.nb_arms)]
         self.received_rewards_weights = [[] for k in range(self.nb_arms)]
 
     def update(self, arm_id, reward, var):
@@ -456,6 +457,18 @@ class ModifiedEpsilonGreedyAlgo():
         self.received_rewards_weights[arm_id - 1].append(weight)
         self.est_means[arm_id - 1] = np.average(self.received_rewards[arm_id - 1], weights = self.received_rewards_weights[arm_id - 1])
 
+    #def nextAction(self):
+    #    pick = np.random.rand()
+    #    if pick < self.epsilon:
+    #        action = random.sample(self.omega, 1)
+    #        action = action[0]
+    #    else:
+    #        best_mean = max(self.est_means)
+    #        action = self.est_means.index(best_mean) + 1
+    #    self.epsilon = max(self.epsilon*self.decay_rate, self.minimal_epsilon)
+    #    return action, True
+
+    
 class CheatingEpsilonGreedyAlgo():
     def __init__(self, epsilon, decay_rate, omega):
         self.r = 1
