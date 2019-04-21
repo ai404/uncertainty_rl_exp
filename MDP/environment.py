7e153b5485e7a01f95350a1b6318b7f53f74194e
Vincent Mai <vincent.mai@umontreal.ca>
2019-04-21 19:53:36 -0400

    Adding semisparse and dense environments
    Refactored to add computeReward function

--- MDP/environment.py
+++ MDP/environment.py
@@ -13,7 +13,6 @@ class bcolors:
     START = '\033[95m'
     #BLUE = '\033[94m'
     TERMINAL = '\033[92m'
-    COIN = '\033[93m'
     AGENT = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
@@ -21,37 +20,39 @@ class bcolors:
 class TabularEnv(gym.Env):
     metadata = {'render.modes': ['human','ansi']}
     
-    def __init__(self,grid_x=5,grid_y=5,n_coins=0,seed=42, max_steps = 10000):
+    def __init__(self, grid_x = 5, grid_y = 5, seed = 42, max_steps = 10000):
+        # Action and observation spaces
         self.grid_x = grid_x
         self.grid_y = grid_y
         self.action_space = spaces.Discrete(4)# The Space object corresponding to valid actions
         self.observation_space = spaces.Discrete(grid_x*grid_y)# The Space object corresponding to valid observations
 
+        # Initialising state
         self.init_state = np.random.choice(range(grid_x*grid_y))
         self.current_state = self.init_state
 
+        # Step counting
         self.max_steps = max_steps
         self.step_count = 0
 
+        # Others
         self.done = False
-        self.n_coins = n_coins
         self.seed(seed)
+        self.name = "Default"
+
+        # Initialising grid
         self.init_grid()
 
     def init_grid(self):
-        probs = np.ones(self.observation_space.n)/(self.observation_space.n - 1)
-        probs[self.init_state] = 0
+        # Terminal state is drawn as one corner (different from init_state)
         self.terminal_state = self.init_state # Will be changed just afterwards
         while self.terminal_state == self.init_state:  # Making sure terminal state is not initial state
             self.terminal_state =  np.random.choice([0, self.grid_x -1, (self.grid_y-1)*self.grid_x, self.grid_x*self.grid_y - 1]) # 4 corners
-        
-        probs[self.terminal_state] = 0
-        probs/=sum(probs)
-        
-        self.coins_indexes = defaultdict(int)
-        for i in np.random.choice(self.observation_space.n,self.n_coins,p=probs,replace=False):
-            self.coins_indexes[i] = 1
 
+        # Indicator of state passed
+        self.passed = [0 for i in range(self.grid_x*self.grid_y)]
+        self.passed[self.init_state] = 1
+        
     def _idx2coords(self,index):
         return index%self.grid_x,index//self.grid_x
     
@@ -59,12 +60,17 @@ class TabularEnv(gym.Env):
         return y*self.grid_x + x
 
     def getCurrentState(self):
+        # State given to the agent: contains both current position and terminal state
         return self.current_state * 10**(np.ceil(np.log10(self.grid_x*self.grid_y))) + self.terminal_state
 
     def step(self, action):
+        # Check that action is legal
         assert self.action_space.contains(action)
+
+        # Increment step count
         self.step_count += 1
 
+        # Do action
         x,y = self._idx2coords(self.current_state)
 
         if action == LEFT:
@@ -77,16 +83,18 @@ class TabularEnv(gym.Env):
             y = max(y-1,0)
 
         self.current_state = self._coords2idx(x,y)
-        
-        reward, reward_noise, reward_var = self._get_reward()
 
+        # Reward (depends if episode is terminated and if so, how)
         if self.current_state == self.terminal_state:
-            #print("Reached terminal_state in " + str(self.step_count))
             reward, reward_noise, reward_var = self.close("term")
-
-        if self.step_count >= self.max_steps:
-            #print("Reached maximal steps number")
+        elif self.step_count >= self.max_steps:
             reward, reward_noise, reward_var = self.close("max_steps")
+        else:
+            reward, reward_noise, reward_var = self._get_reward()
+
+        # Marks that state has been passed before
+        if self.passed[self.current_state] == 0:
+            self.passed[self.current_state] = 1
 
         return self.getCurrentState(), reward, reward_noise, reward_var, self.done
 
@@ -105,9 +113,8 @@ class TabularEnv(gym.Env):
         # A : Agent
         # S : Initial State
         # T : Terminal State
-        # C : a Coin
-        # X : There was a coin in that case
-        # . : Nothing :3
+        # X : Passed state (in this episode)
+        # . : Unpassed state (in this episode)
         ### modes
         # human : for a graphical interface // not implemented
         # ansi  : console based output
@@ -127,9 +134,7 @@ class TabularEnv(gym.Env):
                         print(bcolors.BOLD+bcolors.START+" S "+bcolors.ENDC,end="")
                     elif self.terminal_state == s:
                         print(bcolors.BOLD+bcolors.TERMINAL+" T "+bcolors.ENDC,end="")
-                    elif self.coins_indexes[s] == 1:
-                        print(bcolors.COIN+" C "+bcolors.ENDC,end="")
-                    elif self.coins_indexes[s] == -1:
+                    elif self.passed[s] == 1:
                         print(" X ",end="")
                     else:
                         print(" . ",end="")
@@ -145,41 +150,158 @@ class TabularEnv(gym.Env):
         if seed:
             np.random.seed(seed)
 
+    def getName(self):
+        return self.name
+
+    def computeReward(self, reward_mean, reward_var_mean = None, reward_var_var = None):
+        # Computes the reward, the noise and the variance given the reward mean, the expected reward variance and the variance of the reward variance.
+        if reward_var_mean and reward_var_var:
+            reward_var = drawChiSquare(reward_var_mean, reward_var_var)
+            reward_noise = np.random.normal(loc = 0, scale = np.sqrt(reward_var))
+        elif reward_var_mean and not reward_var_var:
+            reward_var = reward_var_mean
+            reward_noise = np.random.normal(loc = 0, scale = np.sqrt(reward_var))
+        else:
+            reward_var = 0
+            reward_noise = 0
+               
+        return reward_mean, reward_noise, reward_var
+
 
 class SparseTabularEnvironment(TabularEnv):
-    """docstring for SparseTabularEnvironment"""
-    def __init__(self, grid_x=5,grid_y=5, reward_var_mean = None, reward_var_var = None):
+    """This environment gives a final reward of (1000 - number of steps that were taken to arrive there), with noise.
+        Needs as reward parameters:
+            reward_var_mean_ter: mean variance of the reward for the terminal state
+            reward_var_var_ter: variance of the variance of the reward for the terminal state"""
+    def __init__(self, grid_x, grid_y, reward_params):
         super().__init__(grid_x, grid_y)
-        if reward_var_mean:
-            assert reward_var_var, "You need to define variance of reward variance if you define its mean"
-        self.reward_var_mean = reward_var_mean
-        self.reward_var_var = reward_var_var
-    
+        self.reward_var_mean_ter = reward_params["reward_var_mean_ter"]
+        self.reward_var_var_ter = reward_params["reward_var_var_ter"]
+        self.name = "Sparse"  
 
     def _get_reward(self, closing_reason = False):
+        # Max steps
         if closing_reason == "max_steps":
-            return -100000, 0, 0
+            return self.computeReward(-100000)
 
-        elif self.current_state == self.terminal_state:
-            reward_mean = 1000 + self.grid_x*self.grid_y - self.step_count    # size of grid - number of steps
-            
-            if self.reward_var_mean and self.reward_var_var:
-                reward_var = drawChiSquare(self.reward_var_mean, self.reward_var_var)
-                reward_noise = np.random.normal(loc = 0, scale = np.sqrt(reward_var))
-            else:
-                reward_var = None
-                reward = reward_mean
+        # Terminal state
+        elif closing_reason == "term":
+            reward_mean = 1000 - self.step_count # End
+            return self.computeReward(reward_mean, self.reward_var_mean_ter, self.reward_var_var_ter)
 
-            return reward_mean, reward_noise, reward_var
         else:
-            return 0, 0, 1
+            return self.computeReward(0)
+
+
+class SemiSparseTabularEnvironment(TabularEnv):
+    """This environment gives a reward of 1000, with noise, at terminal state and a reward of -1, with noise, at each other state.
+        Needs as reward parameters:
+            reward_var_mean_ter: mean variance of the reward for the terminal state
+            reward_var_var_ter: variance of the variance of the reward for the terminal state
+            reward_var_mean_step: mean variance of the reward at each step
+            reward_var_var_step: variance of the variance of the reward at each step"""    
+    def __init__(self, grid_x, grid_y, reward_params):
+        super().__init__(grid_x, grid_y)
+        self.reward_var_mean_ter = reward_params["reward_var_mean_ter"]
+        self.reward_var_var_ter = reward_params["reward_var_var_ter"]
+        self.reward_var_mean_step = reward_params["reward_var_mean_step"]
+        self.reward_var_var_step = reward_params["reward_var_var_step"]  
+        self.name = "Semi sparse"  
+
+    def _get_reward(self, closing_reason = False):
+        # Max steps
+        if closing_reason == "max_steps":
+            return self.computeReward(-100000)
+
+        # Terminal state
+        elif closing_reason == "term":
+            reward_mean = 1000 # End
+            return self.computeReward(reward_mean, self.reward_var_mean_ter, self.reward_var_var_ter)
+
+        # Normal step
+        else:
+            reward_mean = -1
+            return self.computeReward(reward_mean, self.reward_var_mean_step, self.reward_var_var_step)
+
+class DenseTabularEnvironment(TabularEnv):
+    """This environment gives a reward of 1000 at terminal state and a reward of -1 at each other state.
+        Additionnally, the environment gives a reward between 0 and +10 at each state if it is the first time the agent passes there.
+        Needs as reward parameters:
+            reward_var_mean_ter: mean variance of the reward for the terminal state
+            reward_var_var_ter: variance of the variance of the reward for the terminal state
+            reward_var_mean_step: mean variance of the reward at each step
+            reward_var_var_step: variance of the variance of the reward at each step"""    
+    def __init__(self, grid_x, grid_y, reward_params):
+        super().__init__(grid_x, grid_y)
+        self.reward_var_mean_ter = reward_params["reward_var_mean_ter"]
+        self.reward_var_var_ter = reward_params["reward_var_var_ter"]
+        self.reward_var_mean_step = reward_params["reward_var_mean_step"]
+        self.reward_var_var_step = reward_params["reward_var_var_step"]
+        self.name = "Dense"  
+
+
+    def init_grid(self):
+        # Terminal state is drawn as one corner (different from init_state)
+        self.terminal_state = self.init_state # Will be changed just afterwards
+        while self.terminal_state == self.init_state:  # Making sure terminal state is not initial state
+            self.terminal_state =  np.random.choice([0, self.grid_x -1, (self.grid_y-1)*self.grid_x, self.grid_x*self.grid_y - 1]) # 4 corners
+
+        # Indicator of state passed
+        self.passed = [0 for i in range(self.grid_x*self.grid_y)]
+        self.passed[self.init_state] = 1
+
+        self.state_reward = [np.random.choice(range(11)) for i in range(self.grid_x*self.grid_y)]
+        self.state_reward[self.terminal_state] = 1000
+
+
+    def _get_reward(self, closing_reason = False):
+        # Max steps
+        if closing_reason == "max_steps":
+            return self.computeReward(-100000)
+
+        else:
+            print(self.passed[self.current_state])
+            reward_mean = (1-self.passed[self.current_state])*self.state_reward[self.current_state] - 1
+            return self.computeReward(reward_mean, self.reward_var_mean_step, self.reward_var_var_step)
+
 
 
 if __name__ == "__main__":
-    env = TabularEnv()
+  # Reward parameters
+    rew_var_mean_ter = 100000
+    rew_var_var_ter = 1
+    rew_var_mean_step = 100
+    rew_var_var_step = 1
+    rew_params = {"reward_var_mean_ter": rew_var_mean_ter, "reward_var_var_ter": rew_var_var_ter, "reward_var_mean_step": rew_var_mean_step, "reward_var_var_step": rew_var_var_step}
+    
+    # Environment
+    env = DenseTabularEnvironment(6, 6, rew_params)
     env.render()
-    env.step(1)
-    env.step(1)
-    env.step(1)
-    env.step(1)
+
+    state, reward, reward_noise, reward_var, done = env.step(1)
+    print("Reward: " + str(reward) + ", noise: " + str(reward_noise) + ", var: " + str(reward_var))
+    env.render()
+    
+    state, reward, reward_noise, reward_var, done = env.step(1)
+    print("Reward: " + str(reward) + ", noise: " + str(reward_noise) + ", var: " + str(reward_var))
+    env.render()
+
+    state, reward, reward_noise, reward_var, done = env.step(2)
+    print("Reward: " + str(reward) + ", noise: " + str(reward_noise) + ", var: " + str(reward_var))
+    env.render()
+
+    state, reward, reward_noise, reward_var, done = env.step(2)
+    print("Reward: " + str(reward) + ", noise: " + str(reward_noise) + ", var: " + str(reward_var))
+    env.render()
+
+    state, reward, reward_noise, reward_var, done = env.step(3)
+    print("Reward: " + str(reward) + ", noise: " + str(reward_noise) + ", var: " + str(reward_var))
+    env.render()
+
+    state, reward, reward_noise, reward_var, done = env.step(0)
+    print("Reward: " + str(reward) + ", noise: " + str(reward_noise) + ", var: " + str(reward_var))
+    env.render()
+
+    state, reward, reward_noise, reward_var, done = env.step(0)
+    print("Reward: " + str(reward) + ", noise: " + str(reward_noise) + ", var: " + str(reward_var))
     env.render()
